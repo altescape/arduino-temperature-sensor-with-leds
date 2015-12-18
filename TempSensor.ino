66ced935e550ba069742314ea5e593d59f57b61c
Michael Watts <hello@michaelwatts.me>
2015-12-18 14:15:58 +0000

    Update TempSensor.ino

--- TempSensor.ino
+++ TempSensor.ino
@@ -7,7 +7,7 @@
 //
 // Made by Michael Watts
 // License: CC-BY-SA 3.0
-// Downloaded from: https://123d.circuits.io/circuits/1203766-temp-sensor-test
+// Downloaded from: https://123d.circuits.io/circuits/1387881-temp-sensor-test/
 
 const int TEMP_PIN = 0;
 const int RED_LED = 2;
